8e81a68e8139adbd71825b10e2343d9a9b44bead
ggcarvalho <gbrcarvalho@gmail.com>
2020-02-29 08:15:47 -0300

    Update nim.py

--- nim.py
+++ nim.py
@@ -6,7 +6,7 @@ def remove(row,num,table):
 
 def print_table(table):
     for i in range(len(table)):
-        print(str(i+1)+('  #  '*table[i])+'\n')
+        print(str(i+1)+'   '+(' '*(4-i))+(' O'*table[i])+(' '*(5-i))+'\n')
 
 def change_player(turn):
     if turn%2==0:
@@ -16,15 +16,14 @@ def change_player(turn):
 
 def main():
     table=[1,2,3,4,5]
-
     s=sum(table)
     print('\n')
     print_table(table)
     turn=0
     while s>0:
         change_player(turn)
-        row=int(input('select a row (1-5):'))
-        num=int(input('number of pieces to remove:'))
+        row=int(input('Select a row (1-'+str(len(table))+'): '))
+        num=int(input('Number of pieces to remove: '))
         print('\n')
         table=remove(row,num,table)
         s=sum(table)
@@ -34,5 +33,6 @@ def main():
             print('\n####################')
             change_player(turn-1)
             print('You lose!\n####################')
+
 if __name__ == '__main__':
     main()
